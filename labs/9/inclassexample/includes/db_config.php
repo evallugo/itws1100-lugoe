4b021b012e00ee190c95c54f100fc143adf552f5
evallugo <lugoe@rpi.edu>
2025-04-08 13:29:17 -0400

    try

--- labs/9/inclassexample/includes/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database configuration
-$dbHost = 'localhost';
-$dbUser = 'your_username';  // Replace with your RPI database username
-$dbPass = 'your_password';  // Replace with your RPI database password
-$dbName = 'iit';
-?> 
