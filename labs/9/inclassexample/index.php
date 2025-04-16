b9c664e23f2d8b8d5694d3d2e327f043228ea1c1
evallugo <lugoe@rpi.edu>
2025-04-15 23:20:33 -0400

    update

--- labs/9/inclassexample/index.php
+++ labs/9/inclassexample/index.php
@@ -21,7 +21,7 @@ $dbOk = false;
 
 /* Create a new database connection object, passing in the host, username,
      password, and database to use. The "@" suppresses errors. */
-@$db = new mysqli('localhost', 'root', 'PRLugo22', 'iitF23');
+@$db = new mysqli('localhost', 'root', 'PRLugo22!', 'iitF23');
 
 if ($db->connect_error) {
    echo '<div class="messages">Could not connect to the database. Error: ';
