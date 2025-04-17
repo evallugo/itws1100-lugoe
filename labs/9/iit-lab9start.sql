5fec20b588962692a9c3f29ce952a525fdf886ae
evallugo <lugoe@rpi.edu>
2025-04-17 00:05:12 -0400

    fixed

--- labs/9/iit-lab9start.sql
+++ labs/9/iit-lab9start.sql
@@ -16,3 +16,17 @@ VALUES (1, "Elizabeth", "1998"),
       "2001"
    ),
    (5, "Up in the Air", "2009");
+
+-- insert data into the actors table
+INSERT INTO actors (first_name, last_name, dob)
+VALUES 
+   ("Eddie", "Murphy", "1955-04-03"),
+   ("Danny", "DeVito", "1944-11-17"),
+   ("Tom", "Hanks", "1956-07-09"),
+   ("Leonardo", "DiCaprio", "1958-11-11"),
+   ("Morgan", "Freeman", "1937-06-01");
+
+   INSERT INTO `relationship` (`movieid`, `actorid`) VALUES
+(8,1),
+()
+
