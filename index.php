3f95ac8fbc0a0b4ed4692ad37da099d0984220d3
evallugo <lugoe@rpi.edu>
2025-04-20 18:14:47 -0400

    test

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+//set page variables
+$page_title = "Eva Lugo";
+$page_class = "index";
+
+//include the required files
+include('Quiz3/includes/head.inc.php');
+?>
+
+<div class="center">
+  <div class="center-content">
+    <h1>Eva Lugo</h1>
+    <div class="buttons">
+      <a href="labs/3/aboutme.html">
+        About Me <i class="fa-solid fa-user"></i>
+      </a>
+      <a href="labs/3/resume.html">
+        Resume <i class="fa-solid fa-file"></i>
+      </a>
+      <a href="labs/3/labs.html">
+        Labs <i class="fa-solid fa-folder"></i>
+      </a>
+      <a href="https://github.com/evallugo" target="_blank" rel="noopener noreferrer">
+        GitHub <i class="fa-brands fa-github"></i>
+      </a>   
+      <a href="https://www.linkedin.com/in/eva-lugo" target="_blank" rel="noopener noreferrer">
+        LinkedIn <i class="fa-brands fa-linkedin"></i>
+      </a>       
+    </div>
+  </div>
+</div>
+
+<?php include('Quiz3/includes/foot.inc.php'); ?> 
