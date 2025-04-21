760f785987099081e80416fc94b6936dc2ca1c25
evallugo <lugoe@rpi.edu>
2025-04-21 04:35:07 -0400

    test

--- Quiz3/includes/head.inc.php
+++ Quiz3/includes/head.inc.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
     <!-- Use absolute path from web root -->
-    <link rel="stylesheet" href="/itws1100-lugoe/iit/Quiz3/styles.css">
+    <link rel="stylesheet" href="/iit/Quiz3/styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Alice&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
