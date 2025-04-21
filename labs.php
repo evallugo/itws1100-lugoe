dfc1931f9778f6bdee80926444d1e8ed47d1da9e
evallugo <lugoe@rpi.edu>
2025-04-20 22:47:32 -0400

    test

--- labs.php
+++ labs.php
@@ -11,35 +11,35 @@ require_once __DIR__ . '/Quiz3/includes/head.inc.php';
 require_once __DIR__ . '/Quiz3/conn.php';
 ?>
 
-<div class="labs">
-    <div class="labs-intro">
+<div class="center">
+    <div class="center-content">
         <h1>My Labs</h1>
-        <p>Here are all my labs for Web Systems Development.</p>
-    </div>
-    
-    <div class="lab-grid">
-        <?php
-        // Query to get all labs from the database
-        $query = "SELECT * FROM myLabs ORDER BY lab_number";
-        $result = $db->query($query);
-        
-        if ($result && $result->num_rows > 0) {
-            while ($lab = $result->fetch_assoc()) {
-                $lab_title = "Lab " . $lab['lab_number'];
-                if (!empty($lab['lab_link'])) {
-                    echo '<a href="' . htmlspecialchars($lab['lab_link']) . '" class="lab-button">';
-                    echo '<div class="lab-content">';
-                    echo '<h2 class="lab-title">' . $lab_title . '</h2>';
-                    echo '<i class="fa-solid fa-flask lab-icon"></i>';
-                    echo '<p class="lab-description">' . htmlspecialchars($lab['lab_description']) . '</p>';
-                    echo '</div>';
+        <div class="buttons">
+            <?php
+            // Query to get all labs from the database
+            $query = "SELECT * FROM myLabs ORDER BY lab_number";
+            $result = $db->query($query);
+            
+            if ($result && $result->num_rows > 0) {
+                while ($lab = $result->fetch_assoc()) {
+                    echo '<a href="labs/' . $lab['lab_number'] . '/index.html">';
+                    echo 'Lab ' . htmlspecialchars($lab['lab_number']) . ' ';
+                    echo '<i class="fa-solid fa-flask"></i>';
                     echo '</a>';
                 }
+            } else {
+                // If no labs in database, show default labs
+                for ($i = 1; $i <= 10; $i++) {
+                    if ($i != 3) { // Skip lab 3 as it's your website
+                        echo '<a href="labs/' . $i . '/index.html">';
+                        echo 'Lab ' . $i . ' ';
+                        echo '<i class="fa-solid fa-flask"></i>';
+                        echo '</a>';
+                    }
+                }
             }
-        } else {
-            echo '<p class="no-labs">No labs found in the database.</p>';
-        }
-        ?>
+            ?>
+        </div>
     </div>
 </div>
 
