45bdb94651f18007bf7dab21badb7afb949d2f6e
evallugo <lugoe@rpi.edu>
2025-04-21 02:35:11 -0400

    test

--- Quiz3/includes/navigation.php
+++ Quiz3/includes/navigation.php
@@ -1,7 +1,7 @@
-<a href="index.php"><i class="fas fa-home"></i> Home</a>
-<a href="about.php"><i class="fa-solid fa-user"></i> About</a>
-<a href="resume.php"><i class="fa-solid fa-file"></i> Resume</a>
-<a href="labs.php"><i class="fa-solid fa-folder"></i> Labs</a>
+<a href="/iit/Quiz3/index.php"><i class="fas fa-home"></i> Home</a>
+<a href="/iit/Quiz3/about.php"><i class="fa-solid fa-user"></i> About</a>
+<a href="/iit/Quiz3/resume.php"><i class="fa-solid fa-file"></i> Resume</a>
+<a href="/iit/Quiz3/labs.php"><i class="fa-solid fa-folder"></i> Labs</a>
 <a href="https://github.com/evallugo" target="_blank" rel="noopener noreferrer">
     <i class="fa-brands fa-github"></i> GitHub
 </a>
@@ -10,7 +10,7 @@
 </a>
 <?php if(isset($_SESSION['user'])): ?>
     <span class="welcome">Welcome <?php echo htmlspecialchars($_SESSION['user']['name']); ?>!</span>
-    <a href="/iit/logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+    <a href="/iit/Quiz3/logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
 <?php else: ?>
-    <a href="/iit/login.php"><i class="fas fa-sign-in-alt"></i> Login</a>
+    <a href="/iit/Quiz3/login.php"><i class="fas fa-sign-in-alt"></i> Login</a>
 <?php endif; ?>
