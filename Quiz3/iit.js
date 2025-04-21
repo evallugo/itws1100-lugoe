8357471209fee31b637e21497207b86232f3866e
evallugo <lugoe@rpi.edu>
2025-04-21 19:17:19 -0400

    update

--- Quiz3/iit.js
+++ Quiz3/iit.js
@@ -3,27 +3,23 @@ $(document).ready(function() {
     if (confirm("Are you sure you want to delete this lab?")) {
       var curId = $(this).closest("tr").attr("id");
       var labId = curId.split("-")[1];
-      
       $.ajax({
         type: "POST",
         url: "delete_lab.php",
         dataType: "json",
         data: { id: labId },
-        
         success: function(response) {
           if (response.success) {
             $("#" + curId).remove();
             $("#jsMessages").html("<h4>Lab deleted</h4>").show();
-
-            // re-zebra the table
+            // Re-zebra
             $("#labTable tr").removeClass("odd").each(function(i) {
-              if (i % 2 == 1) $(this).addClass("odd");
+              if (i % 2 === 1) $(this).addClass("odd");
             });
           } else {
             alert(response.message);
           }
         },
-        
         error: function(msg) {
           alert(msg.status + " " + msg.statusText);
         }
